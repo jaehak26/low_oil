e42eacade6c9dc7c86e7e784a9e381d8f1aade1c
jaehak26 <jack10829@naver.com>
2022-10-15 17:16:26 +0900

    create project

---
+++ src/test/java/com/example/low_oil/LowOilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.low_oil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowOilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
